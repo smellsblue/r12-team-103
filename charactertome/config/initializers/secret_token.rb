07cbfcd8056cd7ec0f94ec6792ae7f92f5d83d23
Mike Stone <stonemj@gmail.com>
2012-10-12 17:07:59 -0700

    rails new

---
+++ charactertome/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charactertome::Application.config.secret_token = 'f0d04000bcc3f5ee3b99624e2f3ab969d936cdf794c3ed1fc19449bd8a68e29f87c63ec2f54d9ae14b22ec636703a4d8cf2d2d47d82cb44df38a09c622b4d602'
