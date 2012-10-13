59d0556caf4f063291da532efe41150abc0b210d
Mike Stone <stonemj@gmail.com>
2012-10-13 10:29:14 -0700

    Allow saving the various character attributes.

--- charactertome/app/models/tome.rb
+++ charactertome/app/models/tome.rb
@@ -2,6 +2,13 @@ class Tome < ActiveRecord::Base
   attr_accessible :owner_id
   belongs_to :owner, :class_name => "User"
 
+  validates_each :intelligence, :charisma, :strength, :wisdom, :will, :confidence do |record, attr, value|
+    if value.present?
+      record.errors.add attr, "can only be whole number." unless value.to_s =~ /^\d+$/
+      record.errors.add attr, "must be between 0 and 100." if value < 0 || value > 100
+    end
+  end
+
   DEFAULTS = {
     :alignment => 50,
     :alignment_label => "Unknown",
@@ -25,6 +32,18 @@ class Tome < ActiveRecord::Base
       value = self.profession = value || profession
     when "name"
       value = self.name = value || name
+    when "intelligence"
+      value = self.intelligence = value || intelligence
+    when "charisma"
+      value = self.charisma = value || charisma
+    when "strength"
+      value = self.strength = value || strength
+    when "wisdom"
+      value = self.wisdom = value || wisdom
+    when "will"
+      value = self.will = value || will
+    when "confidence"
+      value = self.confidence = value || confidence
     else
       raise "Not allowed to update #{params[:attribute]}"
     end
