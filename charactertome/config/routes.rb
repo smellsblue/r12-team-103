d9626198aa8898b803d29b085e14a8899d4e9566
Mike Stone <stonemj@gmail.com>
2012-10-13 13:26:11 -0700

    Use custom errors to look prettier.

--- charactertome/config/routes.rb
+++ charactertome/config/routes.rb
@@ -1,4 +1,7 @@
 Charactertome::Application.routes.draw do
+  match "/404", :to => "errors#not_found"
+  match "/422", :to => "errors#rejected"
+  match "/500", :to => "errors#internal"
   root :to => "home#index"
 
   resources :tomes, :only => [:show, :update] do
