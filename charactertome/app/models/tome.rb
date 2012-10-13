071ec8e635fc19b4eb445a46030280bc120fdcc5
Mike Stone <stonemj@gmail.com>
2012-10-12 22:00:47 -0700

    Add alignment

--- charactertome/app/models/tome.rb
+++ charactertome/app/models/tome.rb
@@ -3,8 +3,34 @@ class Tome < ActiveRecord::Base
   belongs_to :owner, :class_name => "User"
 
   DEFAULTS = {
+    :alignment => 50,
+    :alignment_label => "Unknown",
     :attribute => 0,
     :name => "Anonymous",
     :profession => "Nerdist"
   }.freeze
+
+  def morality_label
+    return nil unless morality
+
+    if morality <= (100.0 * 1.0 / 3.0)
+      "Chaotic"
+    elsif morality <= (100.0 * 2.0 / 3.0)
+      "Neutral"
+    else
+      "Lawful"
+    end
+  end
+
+  def ethics_label
+    return nil unless ethics
+
+    if morality <= (100.0 * 1.0 / 3.0)
+      "Evil"
+    elsif morality <= (100.0 * 2.0 / 3.0)
+      "Neutral"
+    else
+      "Good"
+    end
+  end
 end
