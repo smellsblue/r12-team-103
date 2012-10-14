31f1ff21d722cc7b6b7d567bda5cdfcd37dd271e
Mike Stone <stonemj@gmail.com>
2012-10-13 20:05:08 -0700

    Starting towards real tasks...

--- charactertome/app/models/goal.rb
+++ charactertome/app/models/goal.rb
@@ -1,8 +1,13 @@
 class Goal < ActiveRecord::Base
   attr_accessible :label
   belongs_to :tome
+  has_many :tasks
 
   def accomplished_percent
-    42
+    if tasks.empty?
+      0
+    else
+      42
+    end
   end
 end
