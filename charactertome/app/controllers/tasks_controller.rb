45aa0ab6233edc28194e5283b7d2872cec50d9fa
Mike Stone <stonemj@gmail.com>
2012-10-13 21:39:08 -0700

    Allow toggling tasks for real.

--- charactertome/app/controllers/tasks_controller.rb
+++ charactertome/app/controllers/tasks_controller.rb
@@ -11,6 +11,15 @@ class TasksController < ApplicationController
   end
 
   def update
-    render :json => { :task_completed_status => "completed" }
+    @tome = Tome.find params[:tome_id]
+    raise "You do not have permission!" if session[:user_id].to_i != @tome.owner.id
+    goal = Goal.find params[:goal_id]
+    raise "That is not the right goal id!" if goal.tome_id != @tome.id
+    task = Task.find params[:id]
+    raise "That is not the right task id!" if task.goal_id != goal.id
+    result = @tome.update_task! task, params
+    render :json => {
+      :task_completed_status => task.completed_status
+    }
   end
 end
--- charactertome/app/models/task.rb
+++ charactertome/app/models/task.rb
@@ -9,4 +9,16 @@ class Task < ActiveRecord::Base
       :not_completed
     end
   end
+
+  def accomplish!
+    self.accomplished = true
+    self.accomplished_at = Time.now
+    save!
+  end
+
+  def unaccomplish!
+    self.accomplished = false
+    self.accomplished_at = nil
+    save!
+  end
 end
